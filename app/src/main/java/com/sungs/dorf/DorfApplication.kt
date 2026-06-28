cf912b672c2f9d2924a4178153b099d8f8c9b3d2
Sungmin <lsm0205l56@gmail.com>
2026-06-28 17:50:30 +0900

    feat: 초기 세팅

--- app/src/main/java/com/sungs/dorf/DorfApplication.kt
+++ app/src/main/java/com/sungs/dorf/DorfApplication.kt
@@ -1,6 +1,9 @@
 package com.sungs.dorf
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
+class DorfApplication : Application() {
 
-class DorfApplication : Application()
+}
